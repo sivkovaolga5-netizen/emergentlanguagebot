b1403f29bebcbf1f5cee7dbe85215ba1f045c5c4
sivkovaolga5-netizen <sivkovaolga5@gmail.com>
2026-05-24 12:56:14 +0200

    Update and rename bot.ry to bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import anthropic
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-GROUP_ID = -1632746931    # 
-TOPIC_ID = 7994             # 
-ALLOWED_USER_ID = 367884670  # 
+GROUP_ID =-1632746931    # 
+TOPIC_ID =7994             # 
+ALLOWED_USER_ID =367884670  # 
 
 async def handle_message(update, context):
     if not update.message:
